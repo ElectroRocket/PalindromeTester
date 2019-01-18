776561acd83cb903a8dc15e1e629e004133156ba
ElectroRocket <46098819+ElectroRocket@users.noreply.github.com>
2019-01-18 13:54:38 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,7 @@
+#Author: Krishaay J
+#Date Written: 18 January 2019
 word = input("Enter Word: ")
 if(str(word) == str(word)[::-1]):
-    print("It is a palindrome")
+    print("Word is a palindrome")
 else:
-    print("It is not a palindrome")
+    print("Word is not a palindrome")
